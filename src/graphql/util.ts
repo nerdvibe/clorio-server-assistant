3e916be51a8f76ba1fd6a46de996ce63d0603837
nerdvibe <6796782+nerdvibe@users.noreply.github.com>
2021-03-13 03:26:21 +0100

    Updated error handling

--- src/graphql/util.ts
+++ src/graphql/util.ts
@@ -1,6 +1,9 @@
 import {ApolloError} from "apollo-server";
 
 export const sendGraphqlError = (error: Error, code: string = '500') => {
-    const errorMessage = error.message.split('Error: ')[1] || 'Unknown error'
+    const errorMessage = error.message.split(/error: /i)[1] || 'Unknown error'
+    if(errorMessage === 'Unknown error') {
+        console.log(error.message);
+    }
     throw new ApolloError(errorMessage, code)
 }
